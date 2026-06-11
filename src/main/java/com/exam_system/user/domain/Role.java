87d629d6d08f3010700bd4d9d06a3ac161b751e3
valentinorzusa <valentin.orzusa2000@gmail.com>
2026-06-11 01:58:37 -0300

    feat: creacion de entidades User y Role con relaciones JPA

---
+++ src/main/java/com/exam_system/user/domain/Role.java
@@ -0,0 +1,17 @@
+package com.exam_system.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/exam_system/user/domain/User.java
@@ -0,0 +1,21 @@
+package com.exam_system.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String username;
+    private String password;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
